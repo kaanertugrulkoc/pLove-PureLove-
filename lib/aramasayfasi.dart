db5abe38787c9522b0f6d95bd2ef32d3846d4362
kaanertugrulkoc <poison_314@hotmail.com>
2025-01-20 20:01:15 +0300

    Create aramasayfasi.dart

---
+++ lib/aramasayfasi.dart
@@ -0,0 +1,18 @@
+import 'package:flutterprojelerim/0.0_widgetlar.dart';
+
+class aramasayfasi extends StatelessWidget {
+  const aramasayfasi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Row(
+          children: [
+            MesajKarti(),
+          ],
+        ),
+      ],
+    );
+  }
+}
