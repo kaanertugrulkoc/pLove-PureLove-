01e28eb89e8b88d552986a432f84fad9e433fadd
kaanertugrulkoc <poison_314@hotmail.com>
2025-01-23 23:48:11 +0300

    Update 0.0_widgetlar.dart

--- lib/0.0_widgetlar.dart
+++ lib/0.0_widgetlar.dart
@@ -58,7 +58,7 @@ class MesajKarti extends StatelessWidget {
 
                         //şuanda test için girilen text daha sonra mesajlar yeralacak
                         Text(
-                          " https://www.youtube.com/watch?v=YfFp5688MYk",
+                          " https://www.youtube.com/watch?v=IxPU5jut-k0",
                           style: TextStyle(
                             fontStyle: FontStyle.italic,
                             fontWeight: FontWeight.bold,
