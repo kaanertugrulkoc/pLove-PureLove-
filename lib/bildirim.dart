e4e0e513317238c765d471f00af86c6774ec3106
kaanertugrulkoc <poison_314@hotmail.com>
2025-01-24 19:22:58 +0300

    update

---
+++ lib/bildirim.dart
@@ -0,0 +1,17 @@
+import 'package:flutterprojelerim/0.0_widgetlar.dart';
+
+class AramaSayfasi extends StatelessWidget {
+  const AramaSayfasi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'bildirim sayfası ',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
